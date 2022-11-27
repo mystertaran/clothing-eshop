988536dbf8c60b0bd1f6051f05d8fdeff6d0ec9a
mystertaran <sz.taranczewski@gmail.com>
2022-11-27 12:01:29 +0100

    small visual changes + creating sign in form component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,7 +14,10 @@ const Navigation = () => {
         </Link>
 
         <div className="nav-links-container">
-          <Link className="nav-link" to="/sign-in">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
