fca44d1bdaa17fd5c84dad959a7625ec454fc496
mystertaran <sz.taranczewski@gmail.com>
2023-01-02 22:16:57 +0100

    implementing redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -16,9 +16,11 @@ export const USER_ACTION_TYPES = {
   SET_CURRENT_USER: "SET_CURRENT_USER",
 };
 
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
@@ -30,10 +32,6 @@ const userReducer = (state, action) => {
     default:
       throw new Error(`Unhandled type ${type} in userReducer`);
   }
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
 };
 
 export const UserProvider = ({ children }) => {
